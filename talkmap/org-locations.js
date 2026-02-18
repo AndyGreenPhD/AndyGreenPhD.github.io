23ac6b6b764527abdb2df3a4e64f3cfc0b7d2b07
Andy Green <agreen57@kennesaw.edu>
2026-02-18 03:05:04 -0500

    map refinement

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,12 +1,7 @@
 var addressPoints = [
   [
-    "PRESENTATION TITLE GOES HERE<br />Holy Trinity Greek Orthodox Church; Charleston, SC, USA",
-    32.7884363,
-    -79.9399309
-  ],
-  [
-    "Cybersecurity Hygiene for Everyone<br />Holy Trinity Greek Orthodox Church; Charleston, SC, USA",
-    32.7884363,
-    -79.9399309
+    "Cybersecurity Hygiene for Everyone<br />Holy Trinity Greek Orthodox Church; 30 Race Street, Charleston, SC, USA",
+    32.7980319,
+    -79.9483662
   ]
 ];
