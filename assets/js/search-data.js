5f451a69d36608231a867b9319ad5ac318afd049
AndyGreenPhD <agreen57@kennesaw.edu>
2026-03-27 00:21:02 +0000

    Deploying to gh-pages from @ AndyGreenPhD/AndyGreenPhD.github.io@258dd70a5b0091d591232c919e17e775df4edb20 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-presentations",
-          title: "Presentations",
-          description: "Presentations",
+        },{id: "nav-talks",
+          title: "Talks",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/presentations/";
+            window.location.href = "/talks/";
           },
         },{id: "nav-cv",
           title: "CV",
